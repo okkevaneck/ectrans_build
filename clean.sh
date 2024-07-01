e8526f6ef359e8ff786237d83d63d4d7aaf17cb5
Okke van Eck <okke.vaneck@bsc.es>
2024-07-01 11:52:27 +0200

    Finalized setup

--- clean.sh
+++ clean.sh
@@ -3,52 +3,48 @@
 # This file cleans up everything generated from installing ecTrans.
 # ------------------------------------------------------------------------------
 
-# Global color definitions.
-NC='\033[0m'
-GREEN='\033[1;32m'
+# Load helpers for color printing.
+source helpers/helpers.sh
 
-# Display success styled message with given argument.
-function success {
-    echo -e "\n$GREEN \u2714 ~ $1$NC\n" 1>&2
-}
+# Load directory structure for installation paths.
+source helpers/dirs.sh
 
-# --
 
 if [[ "$1" == "all" ]]; then
-    rm -rf  sources/* build/* install/*
+    rm -rf  "${SOURCEDIR:?}"/* "${BUILDDIR:?}"/* "${INSTALLDIR:?}"/*
     success "Sucessfully cleaned ecTrans:  sources/ build/ install/"
 elif [[ "$1" == "sources" ]]; then
     # If no arguments passed, clean all sources dirs.
     if [ $# -eq 1 ]; then
-        rm -rf sources/*
+        rm -rf "${SOURCEDIR:?}"/*
         success "Sucessfully cleaned ecTrans:  sources/"
     else
         # Else remove specified folder.
-        rm -rf "sources/$2"
+        rm -rf "${SOURCEDIR:?}/$2"
         success "Sucessfully cleaned ecTrans:  sources/$2"
     fi
 elif [[ "$1" == "build" ]]; then
     # If no arguments passed, clean all build dirs.
     if [ $# -eq 1 ]; then
-        rm -rf build/*
+        rm -rf "${BUILDDIR:?}"/*
         success "Sucessfully cleaned ecTrans:  build/"
     else
         # Else remove specified folder.
-        rm -rf "build/$2"
+        rm -rf "${BUILDDIR:?}/$2"
         success "Sucessfully cleaned ecTrans:  build/$2"
     fi
 elif [[ "$1" == "install" ]]; then
     # If no arguments passed, clean all installation dirs.
     if [ $# -eq 1 ]; then
-        rm -rf install/*
+        rm -rf "${INSTALLDIR:?}"/*
         success "Sucessfully cleaned ecTrans:  install/"
     else
         # Else remove specified folder.
-        rm -rf "install/$2"
+        rm -rf "${INSTALLDIR:?}/$2"
         success "Sucessfully cleaned ecTrans:  install/$2"
     fi
 else
-    # Default to leaving the install/ dir as you can install different versions.
-    rm -rf sources/* build/* 
+    # Default to leaving the install dir as you can install different versions.
+    rm -rf "${SOURCEDIR:?}"/* "${BUILDDIR:?}"/* 
     success "Sucessfully cleaned ecTrans:  sources/ build/"
 fi
