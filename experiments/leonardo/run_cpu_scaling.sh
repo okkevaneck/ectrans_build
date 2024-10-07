fde4cd9e7e0951a0283e04c956c19a42404ff2a0
Okke van Eck <okke.vaneck@bsc.es>
2024-10-07 14:52:11 +0200

    Updated scaling on Leonardo CPU

--- experiments/leonardo/run_cpu_scaling.sh
+++ experiments/leonardo/run_cpu_scaling.sh
@@ -14,10 +14,10 @@ EXPDIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 # Define experiment details.
 BIN=ectrans-benchmark-cpu-dp
 NITER=10
-NLEV=137
+NLEV=79
 TRUNCATION=1279
 OUTDIR_PREFIX="$EXPDIR/CPU_scaling"
-TIMELIMIT="00:05:00"
+TIMELIMIT="00:10:00"
 NODES="4 8 16 32"
 
 # Schedule a job for each number of nodes.
